a8c66343c7f3894e44325ee0440f35f76c1f4291
GentlemanGit <ivanboltrushko@gmail.com>
2021-04-10 09:15:41 +0300

    feat: comple task 11

--- src/11-encode-line.js
+++ src/11-encode-line.js
@@ -8,8 +8,20 @@
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new Error('Not implemented');
+function encodeLine(str) {
+  const newStr = [];
+  let k = 1;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === str[i + 1]) {
+      k += 1;
+    } else if (str[i] !== str[i + 1]) {
+      if (k !== 1) {
+        newStr.push(k);
+      }
+      newStr.push(str[i]);
+      k = 1;
+    }
+  }
+  return newStr.join('');
 }
-
 module.exports = encodeLine;
